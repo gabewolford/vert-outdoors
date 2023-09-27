dc06b2bb0afc9db59409a0e1d6a1477c8c69c7b4
gabewolford <gabewolford@gmail.com>
2023-09-27 16:58:26 -0700

    changed space between

--- components/BrandBar.vue
+++ components/BrandBar.vue
@@ -11,13 +11,13 @@ defineProps({
   <section>
     <h2 class="text-3xl md:text-4xl"></h2>
     <div
-      class="flex flex-row w-full overflow-x-scroll md:overflow-x-auto lg:overflow-x-auto lg:space-x-6 items-center space-x-8 md:space-x-12"
+      class="flex flex-row w-full overflow-x-scroll md:overflow-x-auto lg:overflow-x-auto items-center space-x-8 md:space-x-12 lg:space-x-10"
     >
       <div v-for="(brand, index) in brands" :key="index" class="flex-1 lg:py-6">
         <img
           :src="brand.logo"
           :alt="brand.name"
-          class="min-w-[115px] md:min-w[90px] lg:min-w-[90px] h-auto object-contain"
+          class="min-w-[115px] md:min-w[90px] lg:min-w-[80px] h-auto object-contain"
         />
       </div>
     </div>
