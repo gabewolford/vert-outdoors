3a995f0395dc18afce12625f78d3c4d80579bda5
gabewolford <gabewolford@gmail.com>
2023-09-28 10:27:39 -0700

    brand card components created, added to brands page

---
+++ components/BrandCardRight.vue
@@ -0,0 +1,55 @@
+<script setup>
+defineProps({
+  brandName: {
+    type: String,
+    required: true,
+  },
+  brandLogo: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  headerText: {
+    type: String,
+    required: true,
+  },
+  bodyText: {
+    type: String,
+    required: true,
+  },
+  bodyText2: {
+    type: String,
+    required: false,
+  },
+  websiteUrl: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <section class="flex flex-col md:flex-row gap-6 lg:gap-8 lg:max-w[900px]">
+    <div class="flex items-center md:w-1/2">
+      <img :src="imageUrl" :alt="brandName" />
+    </div>
+    <div class="flex flex-col gap-6 md:w-1/2 max-h-fit my-auto">
+      <h2 class="hidden">{{ brandName }}</h2>
+      <NuxtLink :to="websiteUrl">
+        <img
+          :src="brandLogo"
+          :alt="brandName"
+          class="max-w-[185px] md:max-w-[250px]"
+        />
+      </NuxtLink>
+      <h3 class="md:text-xl font-medium">{{ headerText }}</h3>
+      <div class="flex flex-col gap-4">
+        <p class="text-sm">{{ bodyText }}</p>
+        <p class="text-sm">{{ bodyText2 }}</p>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/brands.vue
+++ pages/brands.vue
@@ -6,8 +6,65 @@ useHead({
 
 <template>
   <main
-    class="w-[320px] md:w-[85vw] lg:w-[80vw] 2xl:w-[70vw] mx-auto my-auto flex flex-col gap-16 lg:gap-16 pt-6 md:pt-10 pb-[15rem] md:pb-[18rem] lg:pb-[12rem]"
+    class="w-[320px] md:w-[85vw] lg:w-[80vw] 2xl:w-[70vw] mx-auto my-auto flex flex-col gap-16 lg:gap-24 pt-6 md:pt-10 pb-[15rem] md:pb-[18rem] lg:pb-[12rem]"
   >
-    <h1>Brands Page</h1>
+    <BrandCardLeft
+      brandName="Smartwool"
+      brandLogo="/img/brands/brand-card-images/smartwool-logo.png"
+      imageUrl="/img//brands/brand-card-images/smartwool-image.png"
+      headerText="Comfort, confidence, and community to a life lived outside."
+      bodyText="The Smartwool community is a collection of people who believe that time outside is valuable, restorative, and essential for everyone. When Smartwool started making socks back in 1994, their founders simply wanted to be able to ski longer without getting cold toes."
+      bodyText2="That's when they discovered the benefits of Merino wool as a performance fabric. It was soft, moisture-wicking, thermoregulating, sustainable, and odor-neutralizing—helping to keep feet dry, comfortable, and warm longer. It worked so well, in fact, that we use it in almost every single item we make."
+      websiteUrl="https://www.smartwool.com/"
+    />
+    <BrandCardRight
+      brandName="Helly Hansen"
+      brandLogo="/img/brands/brand-card-images/helly-hansen-logo.png"
+      imageUrl="/img//brands/brand-card-images/helly-hansen-image.png"
+      headerText="From our home in Norway, Helly Hansen has been making professional grade gear to help people stay and feel alive for almost 140 years."
+      bodyText="Helly Hansen gear is worn and trusted by professionals on oceans, mountains and worksites. As the #1 apparel brand for Ski Professionals, you’ll find HH at more than 200 ski resort and guiding operations around the world, and worn by more than 55,000 ski professionals. Our sailing gear is worn by world-class sailors across all five oceans, and we’re proud to partner with and participate in some of the sport’s most iconic regattas and sailing associations."
+      websiteUrl="https://www.hellyhansen.com/"
+    />
+    <BrandCardLeft
+      brandName="Clif Bar"
+      brandLogo="/img/brands/brand-card-images/clif-bar-logo.png"
+      imageUrl="/img//brands/brand-card-images/clif-bar-image.png"
+      headerText="A different kind of company."
+      bodyText="We're working to run a different kind of company: The kind of place we'd want to work, that makes the kind of food we'd like to eat, and that strives for a healthier, more sustainable world - the kind of world we'd like to pass on to our children. And those aren't just words. They're our mission statement."
+      websiteUrl="https://www.clifbar.com/"
+    />
+    <BrandCardRight
+      brandName="Camelbak"
+      brandLogo="/img/brands/brand-card-images/camelbak-logo.png"
+      imageUrl="/img//brands/brand-card-images/camelbak-image.png"
+      headerText="An idea born from the most basic human need - thirst."
+      bodyText="Bicycle enthusiast Michael Eidson, is competing in the Hotter'N Hell 100. And that's exactly what it is: a 100-mile road race in the grueling summer heat of Wichita Falls, Texas. Water is vital to surviving the race, and there are few places to refill a water bottle. Eidson, an emergency medical technician by trade, decides to fill an IV bag with water and slip it into a white tube sock. Yes, a tube sock. Then he stuffs the contraption into the back of his bike jersey, throws the thin hose over his shoulder and clamps it with a clothespin."
+      bodyText2="Hands-free hydration is born. And Eidson is able to drink as he pedals...while the other racers laugh and fiddle with their water bottles."
+      websiteUrl="https://www.camelbak.com/"
+    />
+    <BrandCardLeft
+      brandName="BioLite"
+      brandLogo="/img/brands/brand-card-images/biolite-logo.png"
+      imageUrl="/img//brands/brand-card-images/biolite-image.png"
+      headerText="BioLite is on a mission to bring Energy Everywhere™ with revolutionary products that transform the way we Cook, Charge and Light our lives off the grid."
+      bodyText="We are a team of engineers and designers, operators and analysts, story tellers and evangelists, who believe that advanced technologies, built on sustainable business practices, have the power to change the world. Our unique Parallel Innovation business model pairs the needs of families living in energy poverty with the passions of outdoor enthusiasts pushing the boundaries of life outdoors and on the go."
+      websiteUrl="https://www.bioliteenergy.com/"
+    />
+    <BrandCardRight
+      brandName="Helinox"
+      brandLogo="/img/brands/brand-card-images/helinox-logo.png"
+      imageUrl="/img//brands/brand-card-images/helinox-image.png"
+      headerText="Helinox is the global leader of innovative lightweight equipment & furniture."
+      bodyText="Our portable chairs, cots & tables live at the cutting edge of technical design and innovation but are made for living. We make functional beauty designed to enhance your home: wherever you park it, pitch it, build it, rest it or plant it. Wherever you are, we're with you."
+      websiteUrl="https://helinox.com/"
+    />
+    <BrandCardLeft
+      brandName="Therabody"
+      brandLogo="/img/brands/brand-card-images/therabody-logo.png"
+      imageUrl="/img//brands/brand-card-images/therabody-image.png"
+      headerText="Reimagining the way the world looks at wellness and recovery"
+      bodyText="Therabody provides the effective natural solutions you need to take charge of your daily wellness. They have combined education, innovation, and over a decade of pioneering within the tech wellness space to make wellness more accessible for everyone."
+      websiteUrl="https://www.therabody.com/"
+    />
   </main>
 </template>
