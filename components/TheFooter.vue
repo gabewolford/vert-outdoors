2a58b2094613e4c515c2aeb269d544fde5c1a090
gabewolford <gabewolford@gmail.com>
2023-09-28 20:46:40 -0700

    added site credits

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,8 @@
 <template>
-  <footer class="absolute bottom-0 w-full bg-[#15150F] p-4 md:p-6 text-center">
-    <nav class="flex flex-col lg:flex-row justify-between gap-6 items-center">
+  <footer
+    class="absolute bottom-0 w-full bg-[#15150F] p-4 md:p-6 text-center flex flex-col gap-6"
+  >
+    <nav class="flex flex-col lg:flex-row gap-6 items-center justify-between">
       <NuxtLink to="/">
         <img
           src="/img/vert-logo.webp"
@@ -17,5 +19,17 @@
         <NuxtLink to="/contact" class="hover:text-[#3D67C3]">Contact</NuxtLink>
       </div>
     </nav>
+    <div class="flex flex-row justify-between">
+      <h6 class="text-xs">© 2023 VERT Outdoors. All Rights Reserved.</h6>
+      <p class="text-xs">
+        Designed & developed by
+        <NuxtLink
+          to="https://biiigstretch.studio"
+          target="_blank"
+          class="hover:text-[#3D67C3] hover:underline hover:underline-offset-4"
+          >Biiig Stretch Studio</NuxtLink
+        >
+      </p>
+    </div>
   </footer>
 </template>
