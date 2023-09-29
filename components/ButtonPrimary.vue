4f9a24c17a0927c67cf29ba64707aae2ac6f3abe
gabewolford <gabewolford@gmail.com>
2023-09-29 14:22:04 -0700

    changed button text color to white

--- components/ButtonPrimary.vue
+++ components/ButtonPrimary.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <button
-    class="w-fit px-4 py-2 bg-[#2655BB] hover:bg-[#3D67C3] rounded-full text-lg font-medium"
+    class="w-fit px-4 py-2 bg-[#2655BB] hover:bg-[#3D67C3] rounded-full text-white text-lg font-medium"
   >
     {{ buttonText }}
   </button>
