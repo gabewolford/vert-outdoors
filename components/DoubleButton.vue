6070a11397edf7490dbc241dde14e4ea1b9371e3
gabewolford <gabewolford@gmail.com>
2023-09-29 14:45:37 -0700

    double button looking better

--- components/DoubleButton.vue
+++ components/DoubleButton.vue
@@ -20,18 +20,18 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex flex-row w-fit bg-[#635071] rounded-lg">
+  <div class="flex flex-row w-fit border-2 border-[#e2dbaf] rounded-lg">
     <NuxtLink
       :to="leftLinkTo"
       target="_blank"
-      class="w-fit px-4 py-2 hover:underline decoration-2 hover:underline-offset-4 text-white text-md font-medium"
+      class="w-fit px-4 py-2 hover:underline decoration-2 hover:underline-offset-4 text-md font-medium hover:bg-[#e2dbaf] rounded-l-lg hover:rounded-l-lg hover:text-[#1f2c1e] transition-all duration-200"
     >
       {{ buttonTextLeft }}
     </NuxtLink>
-    <div class="border border-white my-1.5"></div>
+    <div class="border border-[#e2dbaf]"></div>
     <NuxtLink
       :to="rightLinkTo"
-      class="w-fit px-4 py-2 hover:underline decoration-2 hover:underline-offset-4 text-white text-md font-medium"
+      class="w-fit px-4 py-2 hover:underline decoration-2 hover:underline-offset-4 text-md font-medium hover:bg-[#e2dbaf] rounded-r-lg hover:rounded-r-lg hover:text-[#1f2c1e] transition-all duration-200"
     >
       {{ buttonTextRight }}
     </NuxtLink>
