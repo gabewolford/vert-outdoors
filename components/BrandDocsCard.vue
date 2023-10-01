e26a4303aaa710d284becc10b742c7c80a1a7124
gabewolford <gabewolford@gmail.com>
2023-10-01 16:31:18 -0700

    document center up and running

---
+++ components/BrandDocsCard.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  brandLogo: {
+    type: String,
+    required: true,
+  },
+  links: {
+    type: Array,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <section class="flex flex-col w-full border-b-2 border-[#e2dbaf] py-4">
+    <div class="flex flex-col md:flex-row text-xl gap-6">
+      <div class="md:w-1/2 pt-2">
+        <h3 class="hidden">{{ name }}</h3>
+        <img class="w-[150px]" :src="brandLogo" :alt="name" />
+      </div>
+
+      <div class="md:w-1/2">
+        <ul class="md:text-right">
+          <li
+            v-for="link in links"
+            :key="link.linkTo"
+            class="p-1 hover:text-blue-500 underline underline-offset-4 text-lg"
+          >
+            <NuxtLink :to="link.linkTo" target="_blank">
+              {{ link.linkText }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/documents.vue
+++ pages/documents.vue
@@ -2,6 +2,135 @@
 useHead({
   title: "Documents | VERT Outdoors",
 });
+
+const clients = [
+  {
+    name: "Smartwool",
+    brandLogo: "/img/brands/brand-card-images/smartwool-logo.png",
+    links: [
+      {
+        linkTo: "mailto:matt@vertoutdoors.com?subject=Smartwool Web Request",
+        linkText: "Become A Dealer",
+      },
+      {
+        linkTo: "https://sales.smartwool.com/",
+        linkText: "Visit B2B Site",
+      },
+      {
+        linkTo: "mailto:Nancy_Longworth@vfc.com",
+        linkText: "Contact Dealer Service",
+      },
+    ],
+  },
+  {
+    name: "Helly Hansen",
+    brandLogo: "/img/brands/brand-card-images/helly-hansen-logo.png",
+    links: [
+      {
+        linkTo: "/img/brands/docs/helly-hansen-dealer-app.pdf",
+        linkText: "Download Dealer Application",
+      },
+      {
+        linkTo: "/img/brands/docs/helly-hansen-credit-app.pdf",
+        linkText: "Download Credit Application",
+      },
+      {
+        linkTo: "https://b2bsport.hellyhansen.com/",
+        linkText: "Visit B2B Site",
+      },
+      {
+        linkTo:
+          "https://production.brandmaster.com/mars/external.enter?p_com_id=11231",
+        linkText: "Brandmaster Image Bank",
+      },
+      {
+        linkTo: "mailto:Stephanie.Dunagan@hellyhansen.com",
+        linkText: "Contact Dealer Service",
+      },
+    ],
+  },
+  {
+    name: "Clif Bar",
+    brandLogo: "/img/brands/brand-card-images/clif-bar-logo.png",
+    links: [
+      {
+        linkTo: "/img/brands/docs/clif-bar-dealer-app.pdf",
+        linkText: "Download Dealer Application",
+      },
+      {
+        linkTo: "/img/brands/docs/clif-bar-credit-app.pdf",
+        linkText: "Download Credit Application",
+      },
+      {
+        linkTo: "https://dealer.clifbar.com/customer/account/login/",
+        linkText: "Visit B2B Site",
+      },
+      {
+        linkTo: "mailto:srfax@clifbar.com",
+        linkText: "Contact Customer Service",
+      },
+    ],
+  },
+  {
+    name: "Camelbak",
+    brandLogo: "/img/brands/brand-card-images/camelbak-logo.png",
+    links: [
+      {
+        linkTo: "mailto:matt@vertoutdoors.com?subject=Camelbak Web Request",
+        linkText: "Become A Dealer",
+      },
+      {
+        linkTo: "https://authorization.camelbak.com/",
+        linkText: "Visit B2B Site",
+      },
+      {
+        linkTo: "mailto:ekarim@camelbak.com",
+        linkText: "Contact Dealer Service",
+      },
+      {
+        linkTo:
+          "https://www.dropbox.com/sh/c89i98sknpwosm8/AADVEc5HLuZDiPYhX8gpmjHja?dl=0",
+        linkText: "2023 Products & Asset Package",
+      },
+      {
+        linkTo: "https://training.camelbak.com/login",
+        linkText: "Sign Up For C.R.E.W. Training",
+      },
+      {
+        linkTo: "https://www.endvr.io/for-sales-associates",
+        linkText: "Retailers: Download ENDVR App",
+      },
+    ],
+  },
+  {
+    name: "BioLite",
+    brandLogo: "/img/brands/brand-card-images/biolite-logo.png",
+    links: [
+      {
+        linkTo: "/img/brands/docs/clif-bar-dealer-app.pdf",
+        linkText: "Download Dealer Application",
+      },
+      {
+        linkTo: "mailto:DealerServices@bioliteenergy.com",
+        linkText: "Contact Dealer Service",
+      },
+    ],
+  },
+  {
+    name: "Helinox",
+    brandLogo: "/img/brands/brand-card-images/helinox-logo.png",
+    links: [
+      {
+        linkTo: "mailto:matt@vertoutdoors.com?subject=Helinox Web Request",
+        linkText: "Become A Dealer",
+      },
+      {
+        linkTo: "mailto:tomiye@helinox.com",
+        linkText: "Contact Dealer Service",
+      },
+    ],
+  },
+];
 </script>
 
 <template>
@@ -11,5 +140,22 @@ useHead({
     <h1 class="w-full text-center text-2xl md:text-5xl font-semibold">
       Document Center
     </h1>
+
+    <!-- <div class="flex flex-col w-full">
+      <div class="md:flex flex-row justify-between text-lg">
+        <h2>Client</h2>
+        <h2>Links</h2>
+      </div>
+    </div> -->
+
+    <div class="flex flex-col w-full">
+      <BrandDocsCard
+        v-for="client in clients"
+        :key="client.name"
+        :name="client.name"
+        :brandLogo="client.brandLogo"
+        :links="client.links"
+      />
+    </div>
   </main>
 </template>
