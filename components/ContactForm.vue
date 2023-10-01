a675145ce715bc1c16a2137dbb42f0f8edcebf4a
gabewolford <gabewolford@gmail.com>
2023-10-01 12:42:38 -0700

    contact form and confirmation done

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -41,68 +41,72 @@ export default {
     @submit.prevent="sendEmail"
     class="flex flex-col w-full mx-auto gap-4 md:text-[#1f2c1e]"
   >
-    <p class="text-xs text-right">* all fields required</p>
+    <p class="text-sm text-right md:hidden lg:block">
+      * All fields are required
+    </p>
 
     <div class="flex flex-col gap-2">
-      <label class="text-sm">First name</label>
+      <label class="text-sm md:text-xs lg:text-sm">First name *</label>
       <input
         type="text"
         placeholder="First name"
         name="first_name"
         required="true"
-        class="p-2 shadow-md"
+        class="p-2 md:p-1 lg:p-2 shadow-md md:text-sm lg:text-base"
       />
     </div>
 
     <div class="flex flex-col gap-2">
-      <label class="text-sm">Last name</label>
+      <label class="text-sm md:text-xs lg:text-sm">Last name *</label>
       <input
         type="text"
         placeholder="Last name"
         name="last_name"
         required="true"
-        class="p-2 shadow-md"
+        class="p-2 md:p-1 lg:p-2 shadow-md md:text-sm lg:text-base"
       />
     </div>
 
     <div class="flex flex-col gap-2">
-      <label class="text-sm">Subject</label>
+      <label class="text-sm md:text-xs lg:text-sm">Subject *</label>
       <input
         type="text"
         placeholder="Subject"
         name="subject"
         required="true"
-        class="p-2 shadow-md"
+        class="p-2 md:p-1 lg:p-2 shadow-md md:text-sm lg:text-base"
       />
     </div>
 
     <div class="flex flex-col gap-2">
-      <label class="text-sm">Email</label>
+      <label class="text-sm md:text-xs lg:text-sm">Email *</label>
       <input
         type="email"
         placeholder="Email"
         name="user_email"
         required="true"
-        class="p-2 shadow-md"
+        class="p-2 md:p-1 lg:p-2 shadow-md md:text-sm lg:text-base"
       />
     </div>
 
     <div class="flex flex-col gap-2">
-      <label class="text-sm">Message</label>
+      <label class="text-sm md:text-xs lg:text-sm">Message *</label>
       <textarea
         name="user_message"
         placeholder="Type your message here"
         required="true"
-        class="p-2 shadow-md"
+        class="p-2 md:p-1 lg:p-2 shadow-md md:text-sm lg:text-base"
       ></textarea>
     </div>
 
-    <button
-      type="submit"
-      :disabled="isSubmitting"
-      class="w-fit px-4 py-2 bg-[#2655BB] hover:bg-[#3D67C3] rounded-full text-white text-lg font-medium"
-    >
-      {{ isSubmitting ? "Sending..." : "Send message" }}
-    </button>
+    <div class="flex flex-row justify-end">
+      <button
+        type="submit"
+        :disabled="isSubmitting"
+        class="w-fit px-4 py-2 bg-[#2655BB] hover:bg-[#3D67C3] rounded-full text-white text-lg md:text-base lg:text-lg font-medium"
+      >
+        {{ isSubmitting ? "Sending..." : "Send message" }}
+      </button>
+    </div>
   </form>
 </template>
