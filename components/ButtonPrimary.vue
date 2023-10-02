fa074f9a5cc68b882364dc819d790590b19aff8d
gabewolford <gabewolford@gmail.com>
2023-10-02 10:02:48 -0700

    404 page works

--- components/ButtonPrimary.vue
+++ components/ButtonPrimary.vue
@@ -4,12 +4,17 @@ defineProps({
     type: String,
     required: true,
   },
+  onClick: {
+    type: String,
+    required: false,
+  },
 });
 </script>
 
 <template>
   <button
     class="w-fit px-4 py-2 bg-[#2655BB] hover:bg-[#3D67C3] rounded-full text-white text-lg font-medium"
+    @click="onClick"
   >
     {{ buttonText }}
   </button>
---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <main class="min-h-screen flex items-center justify-center">
+    <div class="flex flex-col p-4 text-center gap-6">
+      <h1 class="text-2xl md:text-5xl font-semibold">404 Error</h1>
+      <h3>Sorry, that page doesn't exist.</h3>
+      <div class="flex mx-auto">
+        <ButtonPrimary buttonText="Go home" @click="handleError" />
+      </div>
+    </div>
+  </main>
+</template>
