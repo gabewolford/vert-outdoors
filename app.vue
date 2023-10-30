5613603e4e04e40bac1f31ea7fa7472b1f742f35
gabewolford <gabewolford@gmail.com>
2023-10-30 13:11:29 -0700

    added gsap, animated home page

--- app.vue
+++ app.vue
@@ -1,5 +1,47 @@
 <script>
-import "animate.css";
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+
+// onMounted(() => {
+//   gsap.from(".fade-in-up", {
+//     y: 300,
+//     opacity: 0,
+//   });
+//   gsap.to(".fade-in-up", {
+//     duration: 1.5,
+//     y: 0,
+//     opacity: 1,
+//   });
+
+//   gsap.from(".fade-in-left", {
+//     x: 300,
+//     opacity: 0,
+//     scrollTrigger: {
+//       trigger: ".fade-in-left",
+//       start: "top 100%",
+//     },
+//   });
+//   gsap.to(".fade-in-left", {
+//     duration: 1.5,
+//     x: 0,
+//     opacity: 1,
+//   });
+
+//   gsap.from(".fade-in-right", {
+//     x: -300,
+//     opacity: 0,
+//     scrollTrigger: {
+//       trigger: ".fade-in-right",
+//       start: "top 100%",
+//     },
+//   });
+//   gsap.to(".fade-in-right", {
+//     duration: 1.5,
+//     x: 0,
+//     opacity: 1,
+//   });
+// });
 </script>
 
 <template>
