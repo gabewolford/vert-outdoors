a51d2e4b8b0925ce99c344a1454fc0f78305de18
gabewolford <gabewolford@gmail.com>
2023-11-16 14:33:26 -0800

    added preso tool

--- sanity.config.js
+++ sanity.config.js
@@ -1,18 +1,27 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { presentationTool } from "sanity/presentation";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+
+const SANITY_STUDIO_PREVIEW_URL = "http://localhost:3000";
 
 export default defineConfig({
-  name: 'default',
-  title: 'vert-outdoors',
-  basePath: '/admin',
-  projectId: 'zh1t4b0j',
-  dataset: 'production',
+  name: "default",
+  title: "vert-outdoors",
+  basePath: "/admin",
+  projectId: "zh1t4b0j",
+  dataset: "production",
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: SANITY_STUDIO_PREVIEW_URL,
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
