bbe7221c0a2d4d961ea3a633ccb7d6575e7f6d4a
gabewolford <gabewolford@gmail.com>
2023-11-16 14:23:05 -0800

    added sanity presentation

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -1,7 +1,10 @@
 import {defineConfig} from 'sanity'
+import {presentationTool} from 'sanity/presentation'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+
+const SANITY_STUDIO_PREVIEW_URL = 'http://localhost:3000'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +13,13 @@ export default defineConfig({
   projectId: 'zh1t4b0j',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    presentationTool({
+      previewUrl: SANITY_STUDIO_PREVIEW_URL,
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
