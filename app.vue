e5676667d768ee96a992402e11f7ea43fa6b0983
gabewolford <gabewolford@gmail.com>
2023-11-30 10:50:58 -0800

    added icon

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
-<script setup></script>
+<script setup>
+useHead({
+  title: "VERT Outdoors",
+  meta: [{ name: "VERT Outdoors", content: "Outdoor" }],
+  link: [{ rel: "icon", type: "image/svg", href: "/img/small-logo.svg" }],
+});
+</script>
 
 <template>
   <NuxtLayout>
