b35e36cdf3075e99828de5dbfd8c65934973c21b
gabewolford <gabewolford@gmail.com>
2023-12-13 13:33:59 -0800

    cleaned up brand bar

--- components/BrandBar.vue
+++ components/BrandBar.vue
@@ -10,15 +10,15 @@ defineProps({
 <template>
   <section data-aos="fade-up" data-aos-once="true" data-aos-duration="800">
     <div
-      class="flex flex-row w-full overflow-x-scroll md:overflow-x-auto lg:overflow-x-auto items-center space-x-8 md:space-x-12 lg:space-x-10"
+      class="flex flex-row flex-nowrap w-full overflow-x-auto items-center space-x-8 md:space-x-12 lg:space-x-10"
     >
-      <div v-for="(brand, index) in brands" :key="index" class="flex-1 lg:py-6">
-        <img
-          :src="brand.imageUrl"
-          :alt="brand.altText"
-          class="min-w-[115px] md:min-w[90px] lg:min-w-[80px] h-auto object-contain"
-        />
-      </div>
+      <img
+        v-for="(brand, index) in brands"
+        :key="index"
+        :src="brand.imageUrl"
+        :alt="brand.altText"
+        class="lg:py-6 min-w-[115px] md:min-w[90px] lg:min-w-[80px] h-auto object-contain"
+      />
     </div>
   </section>
 </template>
