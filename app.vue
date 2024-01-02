744e804a8a987595d4212efdccae2293b58c01fc
gabewolford <gabewolford@gmail.com>
2024-01-02 14:36:58 -0800

    added seo meta, open graph meta

--- app.vue
+++ app.vue
@@ -1,4 +1,19 @@
 <script setup>
+useSeoMeta({
+  title: "VERT Outdoors",
+  ogTitle: "VERT Outdoors",
+  description:
+    "Elevate your outdoor experience. We proudly represent the most iconic brands in specialty outdoor, ski, bike, and beyond in the Mid-Atlantic region. Extensive retail, product design, merchandising, and marketing knowledge combined with a love of the outdoors allows us to bring an unparalleled depth of service to the you, helping brands and retailers thrive in the changing economy.",
+  ogDescription:
+    "Elevate your outdoor experience. We proudly represent the most iconic brands in specialty outdoor, ski, bike, and beyond in the Mid-Atlantic region. Extensive retail, product design, merchandising, and marketing knowledge combined with a love of the outdoors allows us to bring an unparalleled depth of service to the you, helping brands and retailers thrive in the changing economy.",
+  ogImage: "https://vert-outdoors.vercel.app/img/og-image/png",
+  twitterCard: "summary_large_image",
+  twitterImage: "https://vert-outdoors.vercel.app/img/og-image/png",
+  twitterTitle: "VERT Outdoors",
+  twitterDescription:
+    "Elevate your outdoor experience. We proudly represent the most iconic brands in specialty outdoor, ski, bike, and beyond in the Mid-Atlantic region. Extensive retail, product design, merchandising, and marketing knowledge combined with a love of the outdoors allows us to bring an unparalleled depth of service to the you, helping brands and retailers thrive in the changing economy.",
+});
+
 useHead({
   title: "VERT Outdoors",
   meta: [{ name: "VERT Outdoors", content: "Outdoor" }],
