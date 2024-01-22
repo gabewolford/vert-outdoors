8d0fb56495daeb86e300e7e03a280d5b41887bd9
gabewolford <gabewolford@gmail.com>
2024-01-22 15:06:23 -0800

    updated threshold to fade in

--- src/app/components/BrandBar.jsx
+++ src/app/components/BrandBar.jsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 
 export default function BrandBar({ brands }) {
   return (
-    <section data-aos="fade-up" data-aos-once="true" data-aos-duration="800">
+    <section
+      data-aos="fade-in"
+      data-aos-once="true"
+      data-aos-duration="800"
+      data-aos-offset="0"
+    >
       <div className="grid gap-12 grid-cols-2 md:grid-cols-4 lg:grid-cols-7  items-center">
         {brands.map((brand, index) => (
           <Image
